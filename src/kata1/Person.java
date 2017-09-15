410cbf36d7fea7a14d64b7cdc394ced370fa8ee8
usuario <usuario@10.22.148.67>
2017-09-15 11:21:48 +0100

    Kata1 version 2: se usa Calendar

--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -1,12 +1,14 @@
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Person {
     private final String name;
-    private final Date birthday;
+    private final Calendar birthday;
+    private final long MILLISECONDS_PER_YEAR = (long) (365.25*24*60*60*1000);
 
-    public Person(String name, Date birthday) {
+    public Person(String name, Calendar birthday) {
         this.name = name;
         this.birthday = birthday;
     }
@@ -15,13 +17,18 @@ public class Person {
         return name;
     }
 
-    public Date getBirthday() {
+    public Calendar getBirthday() {
         return birthday;
     }
     
     public int getAge(){
-        return (int) ((new Date().getTime() - 
-                birthday.getTime())/31536000000L);
+        Calendar today = GregorianCalendar.getInstance();
+        return (int) (milliSecondsToYear(today.getTimeInMillis()
+                - birthday.getTimeInMillis()));
+    }
+    
+    private long milliSecondsToYear(long milles){
         
+        return (milles/MILLISECONDS_PER_YEAR);
     }
 }
